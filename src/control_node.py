4f511fe85f08606a2035b9ce8c40b72cdb0d1448
aniseongu <87381208+aniseongu@users.noreply.github.com>
2026-04-17 02:25:36 +0900

    Refactor control node to XycarControlNode class

--- src/control_node.py
+++ src/control_node.py
@@ -1,47 +1,55 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: 안성우 (성균관대학교 소프트웨어학과)
+# Project: Xycar ROS Line Tracer - PID Control Node
+
 import rospy
 from std_msgs.msg import Float32
-from geometry_msgs.msg import Twist
+from ackermann_msgs.msg import AckermannDriveStamped
 
-class ControlNode:
+class XycarControlNode:
     def __init__(self):
-        rospy.init_node('control_node', anonymous=True)
+        rospy.init_node('xycar_control_node', anonymous=True)
         
-        # PID 파라미터 (rospy.get_param을 쓰면 launch 파일에서 수정 가능해 더 전문적임)
-        self.kp = rospy.get_param('~kp', 0.01)
-        self.ki = rospy.get_param('~ki', 0.0001)
-        self.kd = rospy.get_param('~kd', 0.005)
+        # 파라미터 서버에서 PID 게인 값 가져오기 (launch 파일에서 수정 가능)
+        self.kp = rospy.get_param('~kp', 0.4)
+        self.ki = rospy.get_param('~ki', 0.001)
+        self.kd = rospy.get_param('~kd', 0.15)
         
         self.prev_error = 0
         self.integral = 0
-        self.base_speed = 0.2  # 기본 전진 속도
+        self.base_speed = 0.5  # Xycar 기본 전진 속도
         
-        # Publisher: 로봇의 바퀴/조향을 제어하는 표준 토픽(cmd_vel)
-        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        # Publisher: Xycar 조향 및 모터 제어 명령 퍼블리시 (Ackermann 타입)
+        self.drive_pub = rospy.Publisher('/xycar_motor', AckermannDriveStamped, queue_size=1)
         
-        # Subscriber: 비전 노드에서 계산한 오차값 구독
+        # Subscriber: 비전 노드에서 발행한 오차값 구독
         self.error_sub = rospy.Subscriber('/line_error', Float32, self.error_callback)
 
     def error_callback(self, msg):
         error = msg.data
         
-        # PID 계산
+        # 1. PID 제어 로직
         self.integral += error
         derivative = error - self.prev_error
+        
         steering_output = (self.kp * error) + (self.ki * self.integral) + (self.kd * derivative)
         self.prev_error = error
         
-        # Twist 메시지 생성 및 퍼블리시
-        twist = Twist()
-        twist.linear.x = self.base_speed       # 직진 속도
-        twist.angular.z = -steering_output     # 조향각 (방향에 따라 부호 반전 필요)
+        # 2. Xycar 구동 메시지 생성
+        drive_msg = AckermannDriveStamped()
+        drive_msg.drive.speed = self.base_speed
         
-        self.cmd_pub.publish(twist)
-        rospy.loginfo(f"Published cmd_vel -> Linear: {twist.linear.x}, Angular: {twist.angular.z}")
+        # 오차 방향에 맞춰 조향각 적용 (하드웨어 세팅에 따라 부호 반전 가능)
+        drive_msg.drive.steering_angle = -steering_output 
+        
+        # 명령 퍼블리시
+        self.drive_pub.publish(drive_msg)
+        rospy.loginfo("PID Steering Output: {:.2f}".format(steering_output))
 
 if __name__ == '__main__':
     try:
-        ControlNode()
+        node = XycarControlNode()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
