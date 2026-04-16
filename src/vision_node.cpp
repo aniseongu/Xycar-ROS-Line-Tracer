09c054b1f757afe3144b269f171494646a6ea19d
aniseongu <87381208+aniseongu@users.noreply.github.com>
2026-04-17 03:02:54 +0900

    Update vision_node.cpp

--- src/vision_node.cpp
+++ src/vision_node.cpp
@@ -1,70 +1,57 @@
-// Author: 안성우 (성균관대학교 소프트웨어학과)
-// Project: Xycar ROS Line Tracer - Vision Perception Node (C++)
-
 #include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
-#include <image_transport/image_transport.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <std_msgs/Float32.h>
+#include <opencv2/opencv.hpp>
+#include <std_msgs/Int32.h>
 
-class XycarVisionNode {
-private:
-    ros::NodeHandle nh_;
-    image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
-    ros::Publisher error_pub_;
-    int threshold_val_;
+class VisionNode {
+    ros::NodeHandle nh;
+    ros::Subscriber sub;
+    ros::Publisher pub;
 
 public:
-    XycarVisionNode() : it_(nh_) {
-        // 카메라 이미지 구독 (/usb_cam/image_raw)
-        image_sub_ = it_.subscribe("/usb_cam/image_raw", 1, &XycarVisionNode::imageCallback, this);
-        // 계산된 오차 퍼블리셔
-        error_pub_ = nh_.advertise<std_msgs::Float32>("/line_error", 10);
-        
-        threshold_val_ = 60; // 이진화 임계값
+    VisionNode() {
+        sub = nh.subscribe("/usb_cam/image_raw", 1, &VisionNode::imgCallback, this);
+        pub = nh.advertise<std_msgs::Int32>("/lane_midpoint", 1);
     }
 
-    void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
-        cv_bridge::CvImagePtr cv_ptr;
-        try {
-            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-        } catch (cv_bridge::Exception& e) {
-            ROS_ERROR("cv_bridge exception: %s", e.what());
-            return;
+    void imgCallback(const sensor_msgs::ImageConstPtr& msg) {
+        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+        cv::Mat frame = cv_ptr->image;
+        cv::Mat gray, blur, edge, roi;
+
+        // 1. 전처리 (Gray -> Blur -> Canny)
+        cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(gray, blur, cv::Size(5, 5), 0);
+        cv::Canny(blur, edge, 60, 150);
+
+        // 2. ROI 설정 (강의 내용 반영)
+        roi = edge(cv::Rect(0, 300, 640, 150));
+
+        // 3. 허프 변환 직선 검출
+        std::vector<cv::Vec4i> lines;
+        cv::HoughLinesP(roi, lines, 1, CV_PI/180, 30, 30, 10);
+
+        // 왼쪽/오른쪽 차선 분리 및 중앙점 계산 로직 (단순화 버전)
+        int l_count = 0, r_count = 0;
+        double l_pos = 0, r_pos = 0;
+
+        for (auto& l : lines) {
+            double slope = (double)(l[3]-l[1])/(l[2]-l[0]);
+            if (slope < -0.1) { l_pos += (l[0]+l[2])/2.0; l_count++; }
+            else if (slope > 0.1) { r_pos += (l[0]+l[2])/2.0; r_count++; }
         }
 
-        cv::Mat gray, blur, thresh, roi;
-        // 1. 전처리 (그레이스케일 & 가우시안 블러)
-        cv::cvtColor(cv_ptr->image, gray, cv::COLOR_BGR2GRAY);
-        cv::GaussianBlur(gray, blur, cv::Size(5, 5), 0);
-
-        // 2. 이진화 (검은색 라인 추출)
-        cv::threshold(blur, thresh, threshold_val_, 255, cv::THRESH_BINARY_INV);
-
-        // 3. ROI 설정 (하단 30% 영역)
-        int h = thresh.rows;
-        int w = thresh.cols;
-        roi = thresh(cv::Rect(0, h * 0.7, w, h * 0.3));
-
-        // 4. 모멘트를 이용한 무게중심 계산
-        cv::Moments m = cv::moments(roi);
-        if (m.m00 > 0) {
-            float cx = m.m10 / m.m00;
-            float error = cx - (w / 2.0); // 중앙과의 오차
-
-            std_msgs::Float32 error_msg;
-            error_msg.data = error;
-            error_pub_.publish(error_msg);
+        if (l_count > 0 && r_count > 0) {
+            std_msgs::Int32 mid_msg;
+            mid_msg.data = (l_pos/l_count + r_pos/r_count) / 2;
+            pub.publish(mid_msg);
         }
     }
 };
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "xycar_vision_node");
-    XycarVisionNode vision_node;
+    ros::init(argc, argv, "vision_node");
+    VisionNode vn;
     ros::spin();
     return 0;
 }
